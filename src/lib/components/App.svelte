32902a1328e9e51ea0abd66cc4129af0b4b8f71c
Charles Yaris <charles.yaris.-nd@disney.com>
2023-08-04 00:22:46 -0500

    added group element.

--- src/lib/components/App.svelte
+++ src/lib/components/App.svelte
@@ -23,34 +23,36 @@
     levelSettings = levels[level - 1]
 
     svgWidth = (rectWidth + padding) * (levelSettings.codeLength + 2)
-    svgHeight = (rectHeight + padding) * levelSettings.maxTurns
+    svgHeight = (rectHeight + padding) * levelSettings.maxTurns + 1
   }
 </script>
 
 {#if levelSettings}
   <svg class="inline-block" width={svgWidth} height={svgHeight}>
-    {#each Array.from({ length: levelSettings.maxTurns }) as d, i}
-      {#each Array.from({ length: levelSettings.codeLength + 2 }) as dd, ii}
-        <rect
-          class="non-reactive"
-          x={ii * (rectWidth + padding)}
-          y={i * (rectHeight + padding)}
-          rx={3}
-          ry={3}
-          width={rectWidth}
-          height={rectHeight}
-          fill={ii >= levelSettings.codeLength ? "rgb(211,211,211)" : "transparent"}
-          stroke="black"
-        />
-        <foreignObject
-          x={ii * (rectWidth + padding) - padding * 0.5}
-          y={i * (rectHeight + padding) - padding * 0.5}
-          width={rectWidth + padding}
-          height={rectHeight + padding}
-          title="hi"
-          use:tooltip
-        />
+    <g transform="translate({0}, {1})">
+      {#each Array.from({ length: levelSettings.maxTurns }) as d, i}
+        {#each Array.from({ length: levelSettings.codeLength + 2 }) as dd, ii}
+          <rect
+            class="non-reactive"
+            x={ii * (rectWidth + padding)}
+            y={i * (rectHeight + padding)}
+            rx={3}
+            ry={3}
+            width={rectWidth}
+            height={rectHeight}
+            fill={ii >= levelSettings.codeLength ? "rgb(211,211,211)" : "transparent"}
+            stroke="black"
+          />
+          <foreignObject
+            x={ii * (rectWidth + padding) - padding * 0.5}
+            y={i * (rectHeight + padding) - padding * 0.5}
+            width={rectWidth + padding}
+            height={rectHeight + padding}
+            title="hi"
+            use:tooltip
+          />
+        {/each}
       {/each}
-    {/each}
+    </g>
   </svg>
 {/if}
