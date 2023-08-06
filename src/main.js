1f51b8f0d96fe02c649792e8435981dca606cbf4
Charles Yaris <charles.yaris.-nd@disney.com>
2023-08-06 14:11:38 -0500

    reordered imports.

--- src/main.js
+++ src/main.js
@@ -1,7 +1,7 @@
+import App from "./lib/components/App.svelte"
+
 import "../node_modules/svelte-lib/src/lib/static/styles/root.css"
 import "../node_modules/svelte-lib/src/lib/static/styles/app.css"
-
-import App from "./lib/components/App.svelte"
 
 let div = document.createElement("div")
 div.classList.add("mastermind")
