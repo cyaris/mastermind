2132312068dbb75bd6fb3f2438752584f2f61bcf
Charles Yaris <charles.yaris.-nd@disney.com>
2023-08-08 17:57:44 -0500

    added app router tests.

---
+++ src/lib/components/AppRouter.svelte
@@ -0,0 +1,17 @@
+<script>
+  import { Router, Route } from "svelte-routing"
+  import Home from "./Home.svelte"
+  import App from "./App.svelte"
+
+  const currentPageBase = window.location.pathname.split("/")[1] || ""
+
+  console.log(currentPageBase)
+</script>
+
+<main>
+  <!-- Use the dynamically calculated base -->
+  <Router base={currentPageBase}>
+    <Route path="/" component={Home} />
+    <Route path="/test" component={App} />
+  </Router>
+</main>
--- src/lib/components/index.js
+++ src/lib/components/index.js
@@ -1,1 +1,3 @@
 export { default as App } from "./App.svelte"
+// export { default as AppRouter } from "./AppRouter.svelte"
+// export { default as Home } from "./Home.svelte"
