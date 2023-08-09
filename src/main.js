834c6f28ee8331d7bd1ca4e1d5bfd1fe0b4e2457
Charles Yaris <charles.yaris.-nd@disney.com>
2023-08-09 16:19:55 -0500

    removed css import/

--- src/main.js
+++ src/main.js
@@ -1,8 +1,5 @@
 import App from "./lib/components/App.svelte"
 
-import "svelte-lib/styles/root.css"
-import "svelte-lib/styles/app.css"
-
 let div = document.createElement("div")
 div.classList.add("mastermind")
 
