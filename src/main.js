70ec85fbefd943f62bfae5b5a1a9a1e74961d82d
Charles Yaris <charles.yaris.-nd@disney.com>
2023-08-09 16:29:25 -0500

    added ssr.js file.

--- src/main.js
+++ src/main.js
@@ -1,14 +1,4 @@
+import { rollupComponent } from "../ssr.js"
 import App from "./lib/components/App.svelte"
 
-import "../node_modules/svelte-lib/src/lib/static/styles/root.css"
-import "../node_modules/svelte-lib/src/lib/static/styles/app.css"
-
-let div = document.createElement("div")
-div.classList.add("mastermind")
-
-let script = document.currentScript
-script.parentNode.insertBefore(div, script)
-
-new App({
-  target: div,
-})
+rollupComponent(App)
