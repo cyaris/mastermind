80fc1453228c4ef1e4002a5e8cc9c2d1a60644b9
Charles Yaris <charles.yaris.-nd@disney.com>
2023-08-10 17:21:18 -0500

    changed border radius.

--- src/lib/components/Button.svelte
+++ src/lib/components/Button.svelte
@@ -5,7 +5,7 @@
 
 <a {href}
   ><button
-    class="cursor-pointer bg-[#f07b41] text-white font-black rounded-xl border-4 border-solid border-transparent hover:border-[#c44536] text-base w-44 h-16 px-4 py-3"
+    class="cursor-pointer bg-[#f07b41] text-white font-black rounded-full border-4 border-solid border-transparent hover:border-[#c44536] text-base w-44 h-16 px-4 py-3"
     >{label}</button
   ></a
 >
