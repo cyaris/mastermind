4a8d2100fb422287252e27246f919b77adf2abaa
Charles Yaris <charles.yaris.-nd@disney.com>
2023-08-10 22:59:45 -0500

    updated margin.

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,7 +2,7 @@
   import { Button } from "svelte-lib/components"
 </script>
 
-<div class="flex flex-col items-center mt-4 mb-8">
+<div class="flex flex-col items-center mt-2 mb-8">
   <Button href="play" label="Play Game" />
   <div class="flex-row mt-2">
     <Button href="development" label="Development" />
