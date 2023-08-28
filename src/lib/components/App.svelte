9abf06a15ce3f49f307c56dd393a8768c14728ca
Charles Yaris <charles.yaris.-nd@disney.com>
2023-08-28 15:50:22 -0500

    adjusted reactivity for code reveal.

--- src/lib/components/App.svelte
+++ src/lib/components/App.svelte
@@ -201,14 +201,13 @@
           </g></svg
         >
       {:else}
-        <div class="flex flex-col items-start my-8">
+        <div class="non-reactive flex flex-col items-start my-8">
           <span class="font-black text-2xl animation-bounce">You {win ? "win" : "lose"}!</span>
           <span>Here's the code:</span>
           <svg class="flex mt-2" width={svgWidth} height={svgHeight / settings.maxTurns}>
             <g transform="translate({1}, {1})">
               {#each colorCode as color, i}
                 <rect
-                  class="non-reactive"
                   x={i * (rectWidth + padding)}
                   y={0}
                   rx={3}
