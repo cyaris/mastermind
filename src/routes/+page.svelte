0ff25b8123c5160d611874a9c1eb53b7899d905b
Charles Yaris <charles.yaris.-nd@disney.com>
2023-09-04 19:38:12 -0500

    bug fix.

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,7 +2,7 @@
   import { Button } from "svelte-lib/components"
 </script>
 
-<div class="flex flex-col items-center mt-2 mb-8">
+<div class="flex flex-col items-center mb-8">
   <Button classes="w-48" href="play" label="Play Game" />
   <div class="grid grid-cols-1 sm:grid-cols-2 gap-x-1 gap-y-2 mt-2">
     <Button classes="w-48" href="development" label="Development" />
--- src/routes/development/+page.svelte
+++ src/routes/development/+page.svelte
@@ -5,7 +5,8 @@
       programming, I took on the challenge of translating the classic board game, Mastermind, into a command line game.
       I originally hoped to integrate this original Python version into my site with Flask. Since Github doesn't support
       Flask, I decided to learn Javascript and create a web version of Mastermind using the D3.js library. The code for
-      both versions of the game are included [here](https://github.com/cyaris/mastermind).
+      both versions of the game are included
+      <a href="https://github.com/cyaris/mastermind" target="_blank">here</a>.
     </p>
   </div>
 </body>
