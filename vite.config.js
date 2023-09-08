4e3568040b967588d26a29067964a047cf630c22
Charles Yaris <charles.yaris.-nd@disney.com>
2023-09-08 14:56:29 -0500

    importing more files from svelte-lib.

--- vite.config.js
+++ vite.config.js
@@ -1,15 +1,3 @@
-import { sveltekit } from "@sveltejs/kit/vite"
-
-/** @type {import('vite').UserConfig} */
-export const config = {
-  debug: true,
-  plugins: [sveltekit()],
-  build: {
-    sourcemap: true,
-  },
-  server: {
-    port: 3000,
-  },
-}
+import config from "svelte-lib/vite.config.js"
 
 export default config
