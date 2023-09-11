79ce34faabeb6ad0677a627bd1224159f66190da
Charles Yaris <charles.yaris.-nd@disney.com>
2023-09-11 13:58:25 -0500

    added $lib reference for App.

--- src/lib/components/Router.svelte
+++ src/lib/components/Router.svelte
@@ -2,7 +2,7 @@
   import { Router, Route } from "svelte-routing"
   import { Button } from "svelte-lib/components"
 
-  import App from "./App.svelte"
+  import { App } from "$lib/components"
   import Home from "../../routes/+page.svelte"
   import Development from "../../routes/development/+page.svelte"
   import Instructions from "../../routes/instructions/+page.svelte"
